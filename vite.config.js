5701d8e684a82b4b879359d1c0e8716328259013
A787589 <khady.bar@atos.net>
2025-03-27 00:39:09 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // racine du projet
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
-    },
-  },
 })
