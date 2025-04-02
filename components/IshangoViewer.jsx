bffdf784e0901e7cc73f8a6e3859afeac5c81a2e
A787589 <khady.bar@atos.net>
2025-04-02 17:41:32 +0200

    new version

--- components/IshangoViewer.jsx
+++ components/IshangoViewer.jsx
@@ -1,22 +1,25 @@
-import React, { useEffect, useState, Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stage } from "@react-three/drei";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Suspense, useEffect, useState } from 'react';
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 
 function Model() {
   const [object, setObject] = useState();
 
   useEffect(() => {
+    const basePath = "/models/Final_Ishango/";
+
     const mtlLoader = new MTLLoader();
-    mtlLoader.setPath("/models/Final_Ishango/");
+    mtlLoader.setPath(basePath);
     mtlLoader.load("Final_Ishango.mtl", (materials) => {
       materials.preload();
-      const loader = new OBJLoader();
-      loader.setMaterials(materials);
-      loader.setPath("/models/Final_Ishango/");
-      loader.load("Final_Ishango.obj", (obj) => {
-        obj.scale.set(0.01, 0.01, 0.01);
+
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.setPath(basePath);
+      objLoader.load("Final_Ishango.obj", (obj) => {
         setObject(obj);
       });
     });
@@ -27,17 +30,13 @@ function Model() {
 
 export default function IshangoViewer() {
   return (
-    <div style={{ height: 400, borderRadius: 12, overflow: "hidden" }}>
-      <Canvas camera={{ position: [1, 1, 2] }}>
-        <ambientLight intensity={1} />
-        <directionalLight position={[2, 2, 2]} intensity={1} />
-        <Suspense fallback={null}>
-          <Stage>
-            <Model />
-          </Stage>
-        </Suspense>
-        <OrbitControls />
-      </Canvas>
-    </div>
+    <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[3, 3, 3]} intensity={1.2} />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
   );
 }
