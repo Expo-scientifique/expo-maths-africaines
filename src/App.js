22a834be4348ec7e931ea1b3c4aae773afe68ed4
A787589 <khady.bar@atos.net>
2025-04-05 01:25:30 +0200

    new version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MathAfriqueThroughTime from './components/MathAfriqueThroughTime';
+
+function App() {
+  return (
+    <div className="App">
+      <MathAfriqueThroughTime />
+    </div>
+  );
+}
+
+export default App;
