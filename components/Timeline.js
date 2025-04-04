bf523f05c23cb9cce00a73b8cbbd8d7810c7d231
A787589 <khady.bar@atos.net>
2025-04-05 01:05:31 +0200

    new version

---
+++ components/Timeline.js
@@ -0,0 +1,11 @@
+
+import { Box, Button } from '@mui/material';
+export default function Timeline() {
+  return (
+    <Box>
+      <Button variant="contained">Os d'Ishango</Button>
+      <Button variant="contained">Papyrus Rhind</Button>
+      <Button variant="contained">Village Ba-ila</Button>
+    </Box>
+  );
+}
--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/globals.css';
-import '../styles/patterns.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
